59101ce84d98e2e051c20889681c92ea1460bf27
Connor McLeod <z5058240@ad.unsw.edu.au>
2018-05-07 15:04:08 +1000

    Initialise repo

---
+++ application.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template, session, redirect, request, url_for
+
+application = Flask(__name__)
+
+
+@application.route('/', methods=['GET', 'POST'])
+def home():
+	return render_template('solai.html')
+
+@application.route('/about', methods=['GET', 'POST'])
+def about():
+	return render_template('about.html')
+
+@application.route('/contact', methods=['GET', 'POST'])
+def contact():
+	return render_template('contact.html')
+
+if __name__ == "__main__":
+	application.static_folder = 'static'
+	application.run(debug=True)
+
