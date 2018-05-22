cd1926de20c344c5943376485c0d0fea322625b0
Connor McLeod <z5058240@ad.unsw.edu.au>
2018-05-22 17:19:04 +1000

    Add daytrader wip

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-from flask import Flask, render_template, session, redirect, request, url_for
+from flask import Flask, render_template, session, redirect, request, url_for, jsonify
+from get_prices import *
+import re
 
 application = Flask(__name__)
 
@@ -12,6 +14,33 @@ def home():
 @application.route('/about', methods=['GET', 'POST'])
 def about():
 	return render_template('about.html')
+
+@application.route('/daytrader', methods=['GET', 'POST'])
+def daytrader():
+	
+	if request.method == "POST":
+
+		if "grab" in request.form:
+			datatype = request.form.get('datatype')
+			interval = request.form.get('interval')
+			ticker = request.form["ticker"]
+			
+			return redirect(url_for('datapage', datatype=datatype,interval=interval,ticker=ticker))
+
+	return render_template('daytrader.html')
+
+@application.route('/daytrader/<ticker>/<datatype>/<interval>', methods=['GET', 'POST'])
+def datapage(datatype,interval,ticker):
+
+	data = get_response(datatype, interval, ticker).json()
+	for item in data:
+		json_type = str(item)
+
+	if request.method == "POST":
+		if "goBack" in request.form:
+			return redirect(url_for('daytrader'))
+
+	return render_template('datapage.html',data=data,ticker=ticker,json_type=json_type)
 
 if __name__ == "__main__":
 	application.static_folder = 'static'
---
+++ get_prices.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import requests, json, sys
+from hidden import *
+
+def get_response(datatype, interval, ticker):
+
+	session = requests.Session()
+	session.max_redirects = 3
+
+	url = "https://www.alphavantage.co/query"
+
+	if datatype == "Intraday":
+		api_fn = "TIME_SERIES_INTRADAY"
+	elif datatype == "Daily":
+		api_fn = "TIME_SERIES_DAILY"
+	elif datatype == "Adjusted Daily":
+		api_fn = "TIME_SERIES_DAILY_ADJUSTED"
+	elif datatype == "FX":
+		api_fn = "CURRENCY_EXCHANGE_RATE"
+
+	api_symbol = str(ticker)
+	api_interval = str(interval)
+	api_outputSize = "compact"
+
+	data = {
+		"function": api_fn,
+		"symbol": api_symbol,
+		"apikey": api_key,
+		"interval": api_interval,
+		"outputsize": api_outputSize
+	}
+
+	response = requests.get(url, params=data)
+	return response
+
+
+# elif datatype == "fx":
+# 	api_fn = "CURRENCY_EXCHANGE_RATE"
+# 	from_currency = str(input("From currency? e.g. AUD\n"))
+# 	to_currency = str(input("To currency? e.g. USD\n"))
+
+# 	data = {
+# 		"function": api_fn,
+# 		"apikey": api_key,
+# 		"from_currency": from_currency,
+# 		"to_currency": to_currency
+# 	}
